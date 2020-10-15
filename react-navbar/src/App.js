a11e8a2e410a52aa31c93ea56aff59c565e3b901
bedrantirak <bdrntrk@gmail.com>
2020-10-15 14:03:45 +0300

    Update App.js

--- react-navbar/src/App.js
+++ react-navbar/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-     <h1>App</h1>
+      <Navbar />
+  
     </div>
     
   );
